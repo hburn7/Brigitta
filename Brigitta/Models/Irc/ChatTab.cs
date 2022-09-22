83dde4c3d5499ef6ec2d330e85e9328bdbe91b0a
hburn7 <hburnett777@gmail.com>
2022-09-22 19:07:33 -0400

    Tab logic changes

--- Brigitta/Models/Irc/ChatTab.cs
+++ Brigitta/Models/Irc/ChatTab.cs
@@ -1,20 +1,41 @@
+using IrcDotNet;
+using System;
+
 namespace Brigitta.Models.Irc;
 
 public class ChatTab
 {
-	public ChatTab(string name)
+	private readonly IrcWrapper _irc;
+
+	public ChatTab(IrcWrapper irc, string name) : this(irc, name, "")
 	{
 		Name = name;
-		ChatLog = "";
 	}
 
-	public ChatTab(string name, string chatLog)
+	public ChatTab(IrcWrapper irc, string name, string chatLog)
 	{
+		_irc = irc;
 		Name = name;
 		ChatLog = chatLog;
+		IsMpLobby = name.StartsWith("#");
+
+		// if (IsMpLobby)
+		// {
+		// 	MpLobby = GetMpLobbyData();
+		// }
 	}
 	
 	public string Name { get; set; }
 	public string ChatLog { get; set; }
-	public override string ToString() => $"ChatTab(Name={Name}, ChatLog={ChatLog})";
+	public bool IsMpLobby { get; set; }
+	public MultiplayerLobby? MpLobby { get; set; }
+	
+	public override string ToString() => Name;
+
+	private MultiplayerLobby GetMpLobbyData()
+	{
+		// Executes !mp settings and parses the result
+		throw new NotImplementedException();
+	}
+
 }
