9a864336b19ec7b3e61f3ae92202c67e7cbf7504
hburn7 <hburnett777@gmail.com>
2022-09-22 20:07:20 -0400

    Tab reorganization

--- Brigitta/ViewModels/PrimaryDisplayViewModel.cs
+++ Brigitta/ViewModels/PrimaryDisplayViewModel.cs
@@ -1,15 +1,18 @@
 // ReSharper disable MemberCanBeMadeStatic.Global
 
 using Avalonia.Controls;
+using Avalonia.Controls.Selection;
 using Brigitta.Models.Irc;
 using Brigitta.Views;
 using NLog;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 
-#pragma warning disable CA2011
+#pragma warning disable CA2011, CA1826
 
 namespace Brigitta.ViewModels;
 
@@ -25,27 +28,49 @@ public class PrimaryDisplayViewModel : ViewModelBase
 	{
 		"/kick", "/ban", "/clear", "/savelog", "/join", "/quit", "/part"
 	};
-
-	private readonly TabManager _tabManager;
+	private int _chatFeedFontSize = 8;
 
 	// ReSharper disable once MemberInitializerValueIgnored
 	// This is only used to work with the designer
 	private List<ChatTab> _chatTabs;
-	private List<ChatTab> _mpLobbyTabs;
-	
-	private string _currentTab;
-	private int _chatFeedFontSize = 8;
+	private string _currentChatDisplay;
+	// private List<ChatTab> _mpLobbyTabs;
+	private readonly ChatTab _currentTab;
+	private ObservableCollection<ChatTab> _selectedTabs = null!;
 
 	public PrimaryDisplayViewModel(IrcWrapper irc)
 	{
-		_tabManager = new TabManager();
-		_chatTabs = _tabManager.Tabs;
-		_mpLobbyTabs = new List<ChatTab>();
-		_currentTab = _tabManager.CurrentTab;
-		
 		Irc = irc;
+		TabManager = new TabManager(irc);
 
-		AddChatTab(new ChatTab("BanchoBot", "BanchoBot Test"));
+		AddChatTab(new ChatTab(irc, "BanchoBot", "BanchoBot Test"));
+		AddChatTab(new ChatTab(irc, "TheOmyNomy", "TheOmyNomy Test"));
+		AddChatTab(new ChatTab(irc, "#mp_87654321", "#mp_87654321 Test"));
+
+		_chatTabs = TabManager.Tabs;
+		// _mpLobbyTabs = new List<ChatTab>();
+		_currentTab = TabManager.CurrentTab;
+		_currentChatDisplay = _currentTab.ChatLog;
+
+		ChatTabSelectionModel = new SelectionModel<ChatTab>();
+		ChatTabSelectionModel.Source = Tabs;
+		ChatTabSelectionModel.SelectionChanged += (_, e) =>
+		{
+			_logger.Trace("Tab selection changed");
+			// Switches to the newly selected tab
+			if (e.SelectedItems.Count > 1 || e.DeselectedItems.Count > 1)
+			{
+				throw new InvalidOperationException("Only one item can be selected and deselected at a time.");
+			}
+
+			if (e.SelectedItems.First() is not ChatTab tab)
+			{
+				throw new InvalidOperationException("Something other than a chat tab was discovered inside the collection.");
+			}
+
+			TabManager.SwitchToTab(tab.Name);
+			CurrentChatDisplay = TabManager.CurrentTab.ChatLog;
+		};
 
 		DequeueLoop();
 	}
@@ -53,20 +78,44 @@ public class PrimaryDisplayViewModel : ViewModelBase
 	// Only used for PrimaryDisplay.axaml -- would never be called in
 	// production as the IrcWrapper is always passed from the initial login page.
 	public PrimaryDisplayViewModel() : this(new IrcWrapper()) {}
+	public TabManager TabManager { get; }
+	public ISelectionModel ChatTabSelectionModel { get; }
 	public IrcWrapper Irc { get; }
-
 	public List<ChatTab> Tabs
 	{
 		get => _chatTabs;
 		set => this.RaiseAndSetIfChanged(ref _chatTabs, value);
 	}
-	
-	public List<ChatTab> MpLobbyTabs
+	/// <summary>
+	///  Should only ever have one element in this collection as only one
+	///  chat tab can be selected at a given time
+	/// </summary>
+	public ObservableCollection<ChatTab> CurrentlySelectedTabs
 	{
-		get => _mpLobbyTabs;
-		set => this.RaiseAndSetIfChanged(ref _mpLobbyTabs, value);
+		get => _selectedTabs;
+		set => this.RaiseAndSetIfChanged(ref _selectedTabs, value);
 	}
-	
+	public int ChatFeedFontSize
+	{
+		get => _chatFeedFontSize;
+		set => this.RaiseAndSetIfChanged(ref _chatFeedFontSize, value);
+	}
+	public string CurrentChatDisplay
+	{
+		get => _currentChatDisplay;
+		set => this.RaiseAndSetIfChanged(ref _currentChatDisplay, value);
+	}
+	public int MpTimerIncrement => 30;
+	public List<string> AutoCompletePhrases => _autoCompletePhrases();
+	public List<ListBoxItem> TeamRed => _teamGenerator();
+	public List<ListBoxItem> TeamBlue => _teamGenerator();
+
+	// public List<ChatTab> MpLobbyTabs
+	// {
+	// 	get => _mpLobbyTabs;
+	// 	set => this.RaiseAndSetIfChanged(ref _mpLobbyTabs, value);
+	// }
+
 	public void AddChatTab(string? name)
 	{
 		if (name == null)
@@ -75,15 +124,15 @@ public class PrimaryDisplayViewModel : ViewModelBase
 			return;
 		}
 
-		AddChatTab(new ChatTab(name));
+		AddChatTab(new ChatTab(Irc, name));
 	}
 
 	public void AddChatTab(ChatTab tab)
 	{
 		_logger.Trace($"Attempting to add tab: {tab}");
 
-		_tabManager.AddTab(tab);
-		Tabs = _tabManager.Tabs;
+		TabManager.AddTab(tab);
+		Tabs = TabManager.Tabs;
 
 		if (Irc.Client.IsConnected)
 		{
@@ -97,24 +146,18 @@ public class PrimaryDisplayViewModel : ViewModelBase
 			}
 		}
 	}
-	
-	public void RemoveChatTab(string name)
-	{
-		throw new NotImplementedException();
-	}
 
-	public void RemoveCurrentTab()
-	{
-		throw new NotImplementedException();
-	}
+	// todo: turn these into actions that are subscribed to by this
+	public void RemoveChatTab(string name) => throw new NotImplementedException();
+	public void RemoveCurrentTab() => throw new NotImplementedException();
 
 	private void RouteToTab(string tab, string message)
 	{
-		var match = _tabManager.GetTab(tab);
+		var match = TabManager.GetTab(tab);
 		if (match == null)
 		{
-			AddChatTab(new ChatTab(tab, message));
-			_logger.Warn($"Attempted to route message to tab that does not exist. " +
+			AddChatTab(new ChatTab(Irc, tab, message));
+			_logger.Warn("Attempted to route message to tab that does not exist. " +
 			             $"Tab: {tab} | Content: {message}");
 
 			return;
@@ -122,23 +165,6 @@ public class PrimaryDisplayViewModel : ViewModelBase
 
 		match.ChatLog += message;
 	}
-
-	public int ChatFeedFontSize
-	{
-		get => _chatFeedFontSize;
-		set => this.RaiseAndSetIfChanged(ref _chatFeedFontSize, value);
-	}
-
-	public string CurrentTab
-	{
-		get => _tabManager.CurrentTab;
-		set => this.RaiseAndSetIfChanged(ref _currentTab, value);
-	}
-	public int MpTimerIncrement => 30;
-	
-	public List<string> AutoCompletePhrases => _autoCompletePhrases();
-	public List<ListBoxItem> TeamRed => _teamGenerator();
-	public List<ListBoxItem> TeamBlue => _teamGenerator();
 
 	public void DequeueLoop() => Task.Run(async () =>
 	{
@@ -150,7 +176,7 @@ public class PrimaryDisplayViewModel : ViewModelBase
 				{
 					return;
 				}
-				
+
 				string display = $"[{result.IrcCommand}] {result.TimeStampPrint} {result.Content}\n";
 
 				if (result.IrcCommand.Command == IrcCodes.PrivateMessage)
@@ -183,7 +209,7 @@ public class PrimaryDisplayViewModel : ViewModelBase
 
 		_logger.Fatal("Dequeue loop ceased");
 	});
-	
+
 	public void LobbySetupWindow()
 	{
 		var window = new LobbySetup
@@ -196,13 +222,13 @@ public class PrimaryDisplayViewModel : ViewModelBase
 			// Get the data context and use it to populate lobby info.
 			_logger.Debug($"Lobby setup window closed. sender={sender} args={args}");
 		};
-		
+
 		window.Show();
 	}
 
 	public void IncreaseChatFeedFontSize() => ChatFeedFontSize++;
 	public void DecreaseChatFeedFontSize() => ChatFeedFontSize--;
-	
+
 	private List<string> _autoCompletePhrases()
 	{
 		var ls = new List<string>();
@@ -257,8 +283,8 @@ public class PrimaryDisplayViewModel : ViewModelBase
 		}
 
 		// This is a disgusting solution but oh well
-		Irc.Client.SendRawMessage($"PRIVMSG {_tabManager.CurrentTab} {text}");
-		Irc.ChatQueue.Enqueue(new ChatMessage(new IrcCommand(IrcCodes.PrivateMessage), text, 
-			Irc.Credentials.Username, _currentTab));
+		Irc.Client.SendRawMessage($"PRIVMSG {TabManager.CurrentTab} {text}");
+		Irc.ChatQueue.Enqueue(new ChatMessage(new IrcCommand(IrcCodes.PrivateMessage), text,
+			Irc.Credentials.Username, _currentTab.Name));
 	}
 }
