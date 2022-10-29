1b9ff5be17582d692a0f0818cc9d5410610a0363
hburn7 <hburnett777@gmail.com>
2022-10-28 22:52:44 -0400

    Removed unnecessary using

--- BrigittaTests/Usings.cs
+++ BrigittaTests/Usings.cs
@@ -1,6 +1,5 @@
 global using NUnit.Framework;
 global using Brigitta;
 global using Brigitta.Models;
-global using Brigitta.Models.Irc;
 global using Brigitta.Styles;
 global using Brigitta.ViewModels;
