2bd6e385c6f57117668dc024ce4f0540bd8bcbd4
hburn7 <hburnett777@gmail.com>
2022-11-11 15:53:34 -0500

    MudBlazor initial commit

---
+++ BrigittaBlazor/Program.cs
@@ -0,0 +1,42 @@
+using BanchoSharp;
+using BanchoSharp.Interfaces;
+using BrigittaBlazor.Auth;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddAuthenticationCore();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<IBanchoClient, BanchoClient>();
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<AuthenticationStateProvider, BrigittaAuthStateProvider>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
