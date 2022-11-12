064ddb11aa8e32f5309405f228392faceaeaab9f
hburn7 <hburnett777@gmail.com>
2022-11-12 10:48:14 -0500

    Basic authentication and proper displaying of chat messages and channels

--- BrigittaBlazor/Program.cs
+++ BrigittaBlazor/Program.cs
@@ -1,9 +1,9 @@
 using BanchoSharp;
 using BanchoSharp.Interfaces;
 using BrigittaBlazor.Auth;
-using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +16,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<ProtectedSessionStorage>();
 builder.Services.AddScoped<IBanchoClient, BanchoClient>();
+
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<AuthenticationStateProvider, BrigittaAuthStateProvider>();
@@ -25,9 +26,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
