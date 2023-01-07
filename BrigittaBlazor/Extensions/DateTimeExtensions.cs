d0a1f926c28e7f3bd5ec792aa9ed0a1156be3afc
hburn7 <hburnett777@gmail.com>
2023-01-06 20:23:34 -0500

    Bug fixes, adds buttons and info for mp lobbies

--- BrigittaBlazor/Extensions/DateTimeExtensions.cs
+++ BrigittaBlazor/Extensions/DateTimeExtensions.cs
@@ -5,4 +5,5 @@ namespace Brigitta.Extensions;
 public static class DateTimeExtensions
 {
 	public static string ToFormattedTimeString(this DateTime time) => $"{time:HH:mm:ss}";
+	public static string ToFormattedDuration(this TimeSpan? ts) => $"{ts:mm\\:ss}";
 }
