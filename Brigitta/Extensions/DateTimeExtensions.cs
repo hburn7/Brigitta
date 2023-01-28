9e29cab1b6e1dca073b176aee21d8abad81133be
hburn7 <hburnett777@gmail.com>
2023-01-27 23:11:18 -0500

    Deleted Brigitta desktop (old client version)

--- Brigitta/Extensions/DateTimeExtensions.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Brigitta.Extensions;
-
-public static class DateTimeExtensions
-{
-	public static string ToFormattedTimeString(this DateTime time) => $"{time:hh:mm:ss}";
-}
--- BrigittaTests/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using NUnit.Framework;
-global using Brigitta;
-global using Brigitta.Models;
-global using Brigitta.Styles;
-global using Brigitta.ViewModels;
