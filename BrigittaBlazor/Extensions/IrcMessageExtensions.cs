09fbaf6adae77bea0441c6d8ce3832b2e64a4192
hburn7 <hburnett777@gmail.com>
2023-01-30 11:02:27 -0500

    Fixed bug where chat logs saved with UTC timestamps would not populate correctly.

--- BrigittaBlazor/Extensions/IrcMessageExtensions.cs
+++ BrigittaBlazor/Extensions/IrcMessageExtensions.cs
@@ -6,6 +6,9 @@ public static class IrcMessageExtensions
 {
 	public static string ToDisplayString(this IIrcMessage m) => 
 		$"{m.Timestamp.ToFormattedTimeString()} {m.Prefix}: {string.Join(" ", m.Parameters)}";
+	
+	public static string ToUTCDisplayString(this IIrcMessage m) =>
+		$"{m.Timestamp.ToUniversalTime().ToFormattedTimeString()} {m.Prefix}: {string.Join(" ", m.Parameters)}";
 
 	public static string ToDisplayString(this IPrivateIrcMessage pm) => 
 		$"{pm.Timestamp.ToFormattedTimeString()} {pm.Sender}: {pm.Content}";
