c878be1786a3feb16dbf0b1249d0a703c35bb037
hburn7 <hburnett777@gmail.com>
2023-03-11 13:44:45 -0500

    preliminary hotkey listening

---
+++ BrigittaBlazor/Utils/HotkeyListener.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace BrigittaBlazor.Utils;
+
+public static class HotkeyListener
+{
+	public static event Action<object> OnHotkeyPressed;
+	
+	[JSInvokable]
+	public static Task OnKeyDown(JsonElement eventObject)
+	{
+		OnHotkeyPressed?.Invoke(eventObject);
+		return Task.CompletedTask;
+	}
+}
