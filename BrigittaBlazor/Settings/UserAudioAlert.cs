5697f9484c04c79a32fb2ad23c85096d49ca95b3
hburn7 <hburnett777@gmail.com>
2023-05-01 09:42:11 -0400

    Remove unnecessary false declaration on MultiplayerLobbySpecific

--- BrigittaBlazor/Settings/UserAudioAlert.cs
+++ BrigittaBlazor/Settings/UserAudioAlert.cs
@@ -5,7 +5,7 @@ public class UserAudioAlert
 	public string Name { get; set; } = string.Empty;
 	public string Path { get; set; } = string.Empty;
 	public bool Enabled { get; set; } = true;
-	public bool MultiplayerLobbySpecific { get; set; } = false;
+	public bool MultiplayerLobbySpecific { get; set; }
 	public double Volume { get; set; } = 0.5;
 	public EventTrigger Trigger { get; set; }
 	public string TriggerWord { get; set; } = string.Empty;
