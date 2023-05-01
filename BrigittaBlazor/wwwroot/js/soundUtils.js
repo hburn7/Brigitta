7da3df878111dbc8a6408f96738e4d9f5b3ea491
hburn7 <hburnett777@gmail.com>
2023-05-01 09:51:34 -0400

    Fixed audio playing rapidly

--- BrigittaBlazor/wwwroot/js/soundUtils.js
+++ BrigittaBlazor/wwwroot/js/soundUtils.js
@@ -1,7 +1,17 @@
-﻿function playSound(soundFile) {
+﻿var lastPlayedTime = 0;
+
+function playSound(soundFile) {
+    var currentTime = Date.now();
+    if (currentTime - lastPlayedTime < 500) {
+        console.log("playSound: " + soundFile + " skipped");
+        return;
+    }
+
     var audio = new Audio("Sounds/" + soundFile);
     audio.volume = 0.5;
     audio.play();
-    
+
     console.log("playSound: " + soundFile + " " + audio.volume);
+
+    lastPlayedTime = currentTime;
 }
